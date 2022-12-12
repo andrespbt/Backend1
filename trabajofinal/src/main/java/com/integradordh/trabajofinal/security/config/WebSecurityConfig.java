39162cf133d2b56f6a3a61e13e3c559bc28a7dec
Fernando Andres Poblete <andrespbt14@hotmail.com>
2022-12-12 14:36:53 -0300

    Arreglo de error 403 en consola

--- trabajofinal/src/main/java/com/integradordh/trabajofinal/security/config/WebSecurityConfig.java
+++ trabajofinal/src/main/java/com/integradordh/trabajofinal/security/config/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -36,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth" , "/h2-console/**").permitAll()
+                .antMatchers("/auth").permitAll()
                 // Create , update , delete, only allowed to ADMIN
                 .antMatchers(HttpMethod.POST,"/patients","/dentists","/appointments").hasAuthority(ADMIN.name())
                 .antMatchers(HttpMethod.PATCH, "/patients/update", "/dentists/update", "/appointments/update").hasAuthority(ADMIN.name())
@@ -44,6 +45,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
     }
 
     @Override
